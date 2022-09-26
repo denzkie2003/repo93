2a3742212766201c712cf20267d839c6b38db6ab
DenZ <83795556+denzkie2003@users.noreply.github.com>
2022-09-26 10:43:53 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,13 @@
 //Prevent webpage from loading within 800pixels wide screen
 window.addEventListener('load', (event) => {
   
-  /*if(screen.width > 800){
+  if(screen.width > 800){
     alert('The site does not support wider screen. Please drog the border of your browser to make it small.');
     //Hide all elements
     document.getElementsByTagName('body')[0].style.display = "none";
   }else{
     
-  }*/
-  let b = document.getElementsByClassName('main');
-  if(b.style.display == 'none'){
-    alert('The site does not support wider screen. Please drog the border of your browser to make it small.');
+  }
 
-
-  }else{}
   
 });
