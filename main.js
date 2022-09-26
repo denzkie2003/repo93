f75c32acdfef37ba1df3e3b4c3c290c3fd4dec41
DenZ <83795556+denzkie2003@users.noreply.github.com>
2022-09-26 10:34:39 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 //Prevent webpage from loading within 800pixels wide screen
 window.addEventListener('load', (event) => {
   
-  if(window.innerWidth >= 800){
+  if(screen.width < 800){
     alert('The site does not support wider screen. Please drog the border of your browser to make it small.');
     //Hide all elements
     document.getElementsByTagName('body')[0].style.display = "none";
